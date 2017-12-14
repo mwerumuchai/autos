7dbc148cc4fa00ad71f0ad104add2663a8e66563
mwerumuchai <mwerumuchai@gmail.com>
2017-12-14 08:08:19 +0300

    import category in the admin

--- motors/admin.py
+++ motors/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Vehicle,VehicleDetails
+from .models import Vehicle,VehicleDetails,Category
 
 admin.site.register(Vehicle)
 admin.site.register(VehicleDetails)
+admin.site.register(Category)
