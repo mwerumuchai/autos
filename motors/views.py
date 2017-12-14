9e7b3eddde6785ad8bcb456ade21739ca9a2a6f9
mwerumuchai <mwerumuchai@gmail.com>
2017-12-14 08:12:21 +0300

    view function for both vehicle and index to display list of vehicles

--- motors/views.py
+++ motors/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from .models import Vehicle,Category
+from django.contrib import messages
+from django.db import transaction
+from django.http import Http404
 
 
 def index(request):
-    return render(request, 'index.html')
+    '''
+    Function that gets a list a vehicles from a category
+    '''
+    category = Category.get_category()
+
+    title = 'Jeep Categories'
+    return render(request, 'index.html', {"category":category})
 
 def logout(request):
     return render(request, 'index.html')
+
+# VEHICLE
+def vehicle(request):
+    '''
+    Function that gets a list of specific vehicles from a category
+    '''
+    vehicles = Vehicle.get_vehicles()
+
+    return render(request, 'vehicle/vehicles.html', {"vehicles":vehicles})
+
+def single_vehicle(request,pk):
+    '''
+    Function that gets a vehicle information  from a specific
+    '''
+    vehicles = Vehicle.get_single_vehicle(pk)
+
+    return render(request, 'vehicle/vehicles_details.html', {"vehicles":vehicles})
+
+# search photos
