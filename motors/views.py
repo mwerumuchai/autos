5e2c901cf696625aa1e69c186571c6cc604aa407
mwerumuchai <mwerumuchai@gmail.com>
2017-12-15 04:39:56 +0300

    create a view function thst gets vehicle details from the models

--- motors/views.py
+++ motors/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from .models import Vehicle,Category
+from .models import Vehicle,Category,VehicleDetails
 from django.contrib import messages
 from django.db import transaction
 from django.http import Http404
@@ -29,13 +29,29 @@ def vehicle(request,category_id):
 
     return render(request, 'vehicle/vehicles.html', {"vehicles":vehicles})
 
-def single_vehicle(request,pk):
-    '''
-    Function that gets a vehicle information  from a specific
-    '''
-    vehicles = Vehicle.get_single_vehicle(pk)
 
-    return render(request, 'vehicle/vehicles_details.html', {"vehicles":vehicles})
+def vehicledetails(request,vehicle_id):
+    '''
+    Functions that gets details of a specific vehicle
+    '''
+
+    vehicledetails = VehicleDetails.objects.filter(vehicle=vehicle_id)
+
+    return render(request, 'vehicle/vehicles_details.html', {"vehicledetails":vehicledetails})
+
+def single_vehicle(request,vehicle_id):
+    '''
+    Function that gets a vehicle information  from a specific type of vehicle
+    '''
+    try:
+        # vehicles = Vehicle.objects.filter(category=category_id)
+        vehicledetails = VehicleDetails.objects.get(vehicle=vehicle_id)
+        print('vehicledetails')
+    except VehicleDetails.DoesNotExist:
+        raise Http404("The Post does not exist")
+
+    return render(request,'vehicle/vehicle_details.html',{"vehicledetails":vehicledetails})
+
 
 # search photos
 def search_results(request):
