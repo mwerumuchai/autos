dc6279e9c0db45d7a7553e9e80a6d978ab807c90
mwerumuchai <mwerumuchai@gmail.com>
2017-12-15 04:39:23 +0300

    create url for search

--- motors/urls.py
+++ motors/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^vehicle/(\d+)$', views.vehicle,name='vehicles'),
-    url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^vehicle/single-vehicle/(\d+)$', views.single_vehicle,name='single'),
+    url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
 ]
 
